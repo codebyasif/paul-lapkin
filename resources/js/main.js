e9fade0875b8af23b69801934e49625c7895fc68
codebyasif/Protfolio <codebyasif@gmail.com>
2022-11-17 11:47:43 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,1 @@
+var mixer=mixitup('.grid-portfolio-container');function openMenu(){document.getElementById('navbar').style.height="100vh";}function closeMenu(){document.getElementById('navbar').style.height="0%";}$(function(){$('.menu-item a, .scroll-down a').on('click', function(){$('html, body').animate({scrollTop: $($.attr(this,'href')).offset().top},1000); return false;});});
